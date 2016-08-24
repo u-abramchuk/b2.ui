850dec160fb91cde8d8c5a412739e5bf3d0a1470
Uladzimir Abramchuk <u.abramchuk@gmail.com>
2016-08-24 17:10:42 +0300

    fix header.tsx

--- src/imports/ui/header/header.tsx
+++ src/imports/ui/header/header.tsx
@@ -3,9 +3,9 @@ import * as React from 'react';
 export default class HeaderComponent extends React.Component<{}, {}>{
     render() {
         return (
-        <header className="page-header">
-          <h1>b2.ui</h1>
-        </header>
+            <header className="page-header">
+                <h1>b2.ui</h1>
+            </header>
         );
     }
 }
