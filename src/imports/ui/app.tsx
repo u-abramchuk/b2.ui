2dcb4d7764fa90fe9ad7e857c8d08fa12e7a0444
Uladzimir Abramchuk <u.abramchuk@gmail.com>
2016-08-25 20:10:50 +0300

    render workspaces in separate component

--- src/imports/ui/app.tsx
+++ src/imports/ui/app.tsx
@@ -1,49 +1,15 @@
 import * as React from 'react';
-import * as reactMixin from 'react-mixin';
-import { ReactMeteorData } from 'meteor/react-meteor-data';
-
-import { Workspace, Workspaces } from '../api/workspaces';
 
 import HeaderComponent from './header/header'
-import WorkspaceComponent from './workspace';
-
-interface AppData {
-  workspaces: Array<Workspace>;
-}
-
+import WorkspacesComponent from './sidebar/workspaces';
 
 // App component - represents the whole app
-@reactMixin.decorate(ReactMeteorData)
 export default class App extends React.Component<any, {}> {
-  
-  data:AppData;
-
-  constructor(props: any) {
-    super(props);
-  }
-
-  getMeteorData() {
-    let query = {};
-
-    return {
-      workspaces: Workspaces.find(query, {sort: {id: 1}}).fetch()
-    }
-  }
-
-  renderWorkspaces() {
-    return this.data.workspaces.map((workspace: Workspace) => (
-      <WorkspaceComponent key={workspace.id} workspace={workspace} />
-    ));
-  }
-
   render() {
     return (
       <div className="container">
         <HeaderComponent />
-
-        <ul>
-          {this.renderWorkspaces() }
-        </ul>
+        <WorkspacesComponent />
       </div>
     );
   }
---
+++ src/imports/ui/sidebar/workspaces.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import * as reactMixin from 'react-mixin';
+import { ReactMeteorData } from 'meteor/react-meteor-data';
+
+import { Workspace, Workspaces } from '../../api/workspaces'
+
+interface WorkspacesData {
+  workspaces: Array<Workspace>;
+}
+
+// Task component - represents a single todo item
+@reactMixin.decorate(ReactMeteorData)
+export default class WorkspacesComponent extends React.Component<any, any> {
+
+  data: WorkspacesData;
+
+  getMeteorData() {
+    return {
+      workspaces: Workspaces.find({}, { sort: { id: 1 } }).fetch()
+    }
+  }
+
+  renderWorkspaces() {
+    return this.data.workspaces.map((workspace: Workspace) => (
+      <li>{workspace.name}</li>
+    ));
+  }
+
+  render() {
+    return this.data.workspaces.length > 0 ? (
+      <ul>
+        { this.renderWorkspaces() }
+      </ul>
+    ) : null;
+  }
+}
