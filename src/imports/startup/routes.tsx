18b9e2fedc9c8f0dd34b729146861a1835b17ac1
Uladzimir Abramchuk <u.abramchuk@gmail.com>
2016-09-10 00:57:08 +0300

    style login

--- src/imports/startup/routes.tsx
+++ src/imports/startup/routes.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Accounts } from 'meteor/std:accounts-ui';
 
 import ApplicationComponent from '../ui/app';
 import LoginComponent from '../ui/login';
 import LogoutComponent from '../ui/logout';
 import WorkspaceCreatorComponent from '../ui/workspaceCreator';
+
+Accounts.ui.config({
+    loginPath: '/',
+    onSignedInHook: () => browserHistory.push('/')
+});
 
 export default class RoutingComponent extends React.Component<{}, {}>  {
     requireAuth(nextState, replace) {
--- src/imports/ui/login.tsx
+++ src/imports/ui/login.tsx
@@ -1,46 +1,8 @@
 import * as React from 'react';
-import * as reactMixin from 'react-mixin';
-import { ReactMeteorData } from 'meteor/react-meteor-data';
-import { IRouterContext, IInjectedProps, browserHistory } from 'react-router';
+import { Accounts } from 'meteor/std:accounts-ui';
 
-import BlazeTemplateComponent from './blazeTemplate';
-
-interface LoginData {
-    isAuthenticated: boolean;
-}
-
-@reactMixin.decorate(ReactMeteorData)
-export default class LoginComponent extends React.Component<IInjectedProps, {}> {
-    context: IRouterContext;
-    data: LoginData;
-
-    static contextTypes: React.ValidationMap<any> = {
-        router: React.PropTypes.object.isRequired
-    }
-
-    getMeteorData() {
-        return {
-            isAuthenticated: Meteor.userId() !== null
-        };
-    }
-
-    ensureAuthenticated() {
-        if (this.data.isAuthenticated) {
-            this.context.router.push('/');
-        }
-    }
-
-    componentWillMount() {
-        this.ensureAuthenticated();
-    }
-
-    componentDidUpdate() {
-        this.ensureAuthenticated();
-    }
-
+export default class LoginComponent extends React.Component<{}, {}> {
     render() {
-        var template = window['Template'];
-
         return (
             <div className="login-page">
                 <div className="container-fluid-full">
@@ -48,7 +10,7 @@ export default class LoginComponent extends React.Component<IInjectedProps, {}> 
                         <div className="row-fluid">
                             <div className="login-box">
                                 <h2>Login to your account</h2>
-                                <BlazeTemplateComponent template={template.loginButtons} />
+                                <Accounts.ui.LoginForm className="form-horizontal" />
                             </div>
                         </div>
                     </div>
