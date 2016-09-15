039cd75cf3a0480d73135e013b6835dffbb7f8cf
Uladzimir Abramchuk <u.abramchuk@gmail.com>
2016-09-15 18:53:02 +0300

    refactor startup

---
+++ src/imports/startup/server/auth.ts
@@ -0,0 +1,31 @@
+import { ServiceConfiguration } from 'meteor/service-configuration';
+
+import '../../api/workspaces';
+
+enum LoginStyle {
+    popup,
+    redirect
+}
+
+export interface AuthSettings {
+    clientId: string;
+    secret: string;
+    loginStyle: LoginStyle
+}
+
+function configureAuth(service: string) {
+  const settings: AuthSettings = Meteor.settings[service];
+  ServiceConfiguration.configurations.upsert({ service: service },
+    {
+      service: service,
+      clientId: settings.clientId,
+      secret: settings.secret,
+      loginStyle: settings.loginStyle
+    });
+}
+
+
+Meteor.startup(() => {
+  configureAuth('github');
+  configureAuth('google');
+});
